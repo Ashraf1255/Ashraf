9851d86afca39c45ea51bfbaf20ceb1fd313f5c1
Ashraf Hossain <hossainashraf38@gmail.com>
2016-04-17 08:31:36 +0600

    file

---
+++ emobile/all_map.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>emobile_Pastrolism</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+ 	<meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    	<div id="head">
+            
+            <h2>pAtH_finDER</h2>
+        
+      </div>
+    	
+        
+    	<div class="main">
+            <div id="head">
+               <h2>Choice</h2>
+            </div>
+    		
+                <div>
+                   <div id="four">
+                     <a href=""><img src="pic/carbon.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/cloud.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/fire.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/land.png"></a>
+                   </div id="four">
+                   <div id="four">
+                     <a href=""><img src="pic/productivity.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/rain.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/snow.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/vegetation.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/water.png"></a>
+                   </div>
+                  
+                </div>      
+            
+    	</div>
+    	
+    </div>
+</body>
+</html>
---
+++ emobile/select.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>emobile_Pastrolism</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+ 	<meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    	<div id="head">
+            
+            <h2>pAtH_finDER</h2>
+        
+      </div>
+    	
+        
+    	<div class="main">
+            <div id="head">
+               <h2>Choice</h2>
+            </div>
+    		
+                <div id="two">
+                   <button onclick="window.location.href='year.php'">
+                    <p>Select By Year</p>
+                   </button>
+                   <button onclick="window.location.href='all.php'">
+                    <p>All</p>
+                   </button>
+                   
+                  
+                </div>      
+            
+    	</div>
+    	
+    </div>
+</body>
+</html>
