ddd441bd7659f19211ad1c807743b490a2e8211f
Ashraf Hossain <hossain1255@outlook.com>
2016-04-28 21:09:41 +0600

    emobile

---
+++ all.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>emobile_Pastrolism</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+      <div id="head">
+            
+            <a href="main.php"><img src="pic/head.png"></a>
+        
+      </div>
+      
+        
+      <div class="main">
+            <div id="head">
+               <h2>Choice</h2>
+            </div>
+        
+                <div>
+                   <div id="four">
+                     <a href=""><img src="pic/one.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/two.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/three.png"></a>
+                   </div>
+                   <div id="four">
+                     <a href=""><img src="pic/four.png"></a>
+                   </div>
+                   
+                </div>      
+            
+      </div>
+      
+    </div>
+</body>
+</html>
